b7c8b99c13ecc385a8c76269d41781dfcf88781e
Rayki <cocoog77@gmail.com>
2022-04-27 14:40:48 +0200

    fixed utils so there's no error when building

--- Mo l/Assets/Scripts/Utils/ExtensionMethods.cs
+++ Mo l/Assets/Scripts/Utils/ExtensionMethods.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using PuzzleCat.Level;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace PuzzleCat.Utils
 {
@@ -17,7 +20,7 @@ namespace PuzzleCat.Utils
                 Mathf.Round(vector3.y),
                 Mathf.Round(vector3.z));
         }
-        
+
         public static Vector3Int ToVector3Int(this Vector3 vector3)
         {
             return new Vector3Int(
@@ -25,7 +28,7 @@ namespace PuzzleCat.Utils
                 Mathf.RoundToInt(vector3.y),
                 Mathf.RoundToInt(vector3.z));
         }
-        
+
         public static int Sum(this Vector3Int vector3)
         {
             return vector3.x + vector3.y + vector3.z;
@@ -41,22 +44,24 @@ namespace PuzzleCat.Utils
                 _ => throw new ArgumentOutOfRangeException(nameof(surface), surface, null)
             };
         }
-        
+
         public static Surface ToSurface(this Vector3 surfaceNormal)
         {
             return surfaceNormal.ToVector3Int().ToSurface();
         }
-        
+
         public static Surface ToSurface(this Vector3Int surfaceNormal)
         {
             if (surfaceNormal == Vector3Int.up)
             {
                 return Surface.Floor;
             }
+
             if (surfaceNormal == Vector3Int.right)
             {
                 return Surface.SideWall;
             }
+
             if (surfaceNormal == Vector3Int.back)
             {
                 return Surface.BackWall;
@@ -68,7 +73,8 @@ namespace PuzzleCat.Utils
 
         #endregion
 
-        #region SerializedProperty
+#if UNITY_EDITOR
+#region SerializedProperty
 
         public static IEnumerable<SerializedProperty> GetChildren(this SerializedProperty property)
         {
@@ -97,33 +103,37 @@ namespace PuzzleCat.Utils
                 }
             }
         }
-        
+
         public static SingleMovable[] GetAsSingleMovableArray(this SerializedProperty prop)
         {
-            if (prop == null) throw new System.ArgumentNullException("prop");
-            if (!prop.isArray) throw new System.ArgumentException("SerializedProperty does not represent an Array.", "prop");
+            if (prop == null) throw new ArgumentNullException("prop");
+            if (!prop.isArray)
+                throw new ArgumentException("SerializedProperty does not represent an Array.", "prop");
 
             var arr = new SingleMovable[prop.arraySize];
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = (SingleMovable) prop.GetArrayElementAtIndex(i).objectReferenceValue;
             }
+
             return arr;
         }
 
         public static void SetAsSingleMovableArray(this SerializedProperty prop, SingleMovable[] arr)
         {
-            if (prop == null) throw new System.ArgumentNullException("prop");
-            if (!prop.isArray) throw new System.ArgumentException("SerializedProperty does not represent an Array.", "prop");
+            if (prop == null) throw new ArgumentNullException("prop");
+            if (!prop.isArray)
+                throw new ArgumentException("SerializedProperty does not represent an Array.", "prop");
 
             int sz = arr?.Length ?? 0;
             prop.arraySize = sz;
-            for(int i = 0; i < sz; i++)
+            for (int i = 0; i < sz; i++)
             {
                 prop.GetArrayElementAtIndex(i).objectReferenceValue = arr[i];
             }
         }
-
-        #endregion
+        
+#endregion
+#endif
     }
 }
