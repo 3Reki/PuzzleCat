2ca276a81e406a3cd706bcb78435bda35f2ca774
Rayki <cocoog77@gmail.com>
2022-05-24 21:14:04 +0200

    levels locked in title screen + progress and settings saved

--- Mo l/Assets/Scripts/TitleScreenMenuManager.cs
+++ Mo l/Assets/Scripts/TitleScreenMenuManager.cs
@@ -1,11 +1,13 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace PuzzleCat
 {
     public class TitleScreenMenuManager : MonoBehaviour
     {
+        [SerializeField] private Button[] levelButtons;
+        
         public static void LoadLevel(int level)
         {
             SceneManager.LoadScene(level);
@@ -14,6 +16,15 @@ namespace PuzzleCat
         private void Awake()
         {
             Application.targetFrameRate = Screen.currentResolution.refreshRate;
+        }
+
+        private void Start()
+        {
+            for (int i = GameData.Instance.unlockedLevelsCount; i < levelButtons.Length; i++)
+            {
+                levelButtons[i].interactable = false;
+            }
+            
         }
     }
 }
